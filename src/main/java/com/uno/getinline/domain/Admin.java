d1d5a97b8c06e12102de51eb86a152c85223e49a
choi <chlwlgns620@naver.com>
2022-02-19 19:29:04 +0900

    Design #2 - domain objects and enums
    개발하면서 기능 입출력에 적용해보기 위해
    코로나 줄서기 erd를 참고하여 필요한 도메인 오브젝트를 설계
    추후 JPA를 사용할 때 참고하거나 활용할 예정

---
+++ src/main/java/com/uno/getinline/domain/Admin.java
@@ -0,0 +1,16 @@
+package com.uno.getinline.domain;
+
+import java.time.LocalDateTime;
+
+public class Admin {
+    private Long id;
+
+    private String email;
+    private String nickname;
+    private String password;
+    private String phoneNumber;
+    private String memo;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifedAt;
+}
