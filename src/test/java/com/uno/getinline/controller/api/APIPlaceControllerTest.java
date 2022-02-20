067b706ed8fcfe6dec4c42dbe6f781a7aff2e073
choi <chlwlgns620@naver.com>
2022-02-20 22:49:28 +0900

    Refactor #2 - APIPlaceControllerTest
    TDD 실습하면서 테스트 작성한 내용을 반영
    
    * 장소 리스트 조회 API 테스트 메소드의 이름을 좀 더 친절하게
    * `contentTypeCompatibleWith()` -> `contentType()` 으로 검사 조건을 보다 특정하게 수정

--- src/test/java/com/uno/getinline/controller/api/APIPlaceControllerTest.java
+++ src/test/java/com/uno/getinline/controller/api/APIPlaceControllerTest.java
@@ -23,7 +23,7 @@ class APIPlaceControllerTest {
     public APIPlaceControllerTest(
             @Autowired MockMvc mvc,
             @Autowired ObjectMapper mapper
-            ) {
+    ) {
         this.mvc = mvc;
         this.mapper = mapper;
     }
@@ -64,10 +64,10 @@ class APIPlaceControllerTest {
 
         // When & Then
         mvc.perform(
-                post("/api/places")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(placeRequest))
-        )
+                        post("/api/places")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(placeRequest))
+                )
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(true))
@@ -129,10 +129,10 @@ class APIPlaceControllerTest {
 
         // When & Then
         mvc.perform(
-                put("/api/places/" + placeId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(placeRequest))
-        )
+                        put("/api/places/" + placeId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(placeRequest))
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(true))
